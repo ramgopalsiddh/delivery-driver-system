38d9656bde338cad72a7da58233ac40aa738102b
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2025-08-15 15:05:44 +0530

    Updates in Auth system

---
+++ backend/app/crud/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: UserCreate, hashed_password: str):
+    db_user = User(username=user.username, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
